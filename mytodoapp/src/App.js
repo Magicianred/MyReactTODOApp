f0d578027e27c9e8f3d62334694dbd274818b512
sony <sony@localhost.localdomain>
2019-12-19 00:14:51 +0000

    background

--- mytodoapp/src/App.js
+++ mytodoapp/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import randomColor from 'randomcolor';
-import { Button, Paper, Card, Typography } from '@material-ui/core';
+import { Button, Paper, Card, Input, CardContent } from '@material-ui/core';
 
 function App() {
-	const [data, handleData] = useState(' ');
+	const [title, handleTitle] = useState('');
+	const [data, handleData] = useState('');
 	const [list, setList] = useState([]);
 
 	const saveItem = () => {
-		setList((e) => [...e, data]);
+		setList((e) => [...e, title + ': ' + data]);
 	};
 
 	const handleKeyDown = (e) => {
@@ -20,6 +21,7 @@ function App() {
 
 	const clearInput = () => {
 		handleData('');
+		handleTitle('');
 	};
 
 	const handleRemoveItem = (id) => {
@@ -31,81 +33,123 @@ function App() {
 		return (
 			<div>
 				{list.map((e, k) => (
-					<Paper>
-						<li key={k}>
-							<Card style={{ backgroundColor: randomColor() }} size="big">
-								<p>
-									{e}
-									<br></br>
-								</p>
-							</Card>
-							<Button color="secondary" onClick={() => handleRemoveItem(k)}>
-								Done
-							</Button>
-						</li>
-					</Paper>
+					<div>
+						<Paper>
+							<li key={k}>
+								<Card raised="true" width="200px">
+									<CardContent
+										style={{
+											textAlign: 'left',
+											backgroundColor: randomColor(),
+											width: window.screen.width
+										}}
+									>
+										<p>{e}</p>
+									</CardContent>
+								</Card>
+
+								<Button
+									onClick={() => handleRemoveItem(k)}
+									size="big"
+									variant="outlined"
+									color="secondary"
+								>
+									Done
+								</Button>
+							</li>
+						</Paper>
+						<br></br>
+						<br></br>
+					</div>
 				))}
 			</div>
 		);
 	};
-
 	const deleteList = () => {
 		setList([]);
 		console.log(list);
 	};
 	return (
-		<div>
-			<div className="App">
-				<h1>My TODO App</h1>
-				<div className="Todo">
-					<div>
-						<br></br>
-						<h1>Input todo</h1>
-						<form>
-							<input
-								onKeyDown={handleKeyDown}
-								onChange={(e) => handleData(e.target.value)}
-								value={data}
-							/>
-							<hr />
-						</form>
-					</div>
+		<div style={{ backgroundColor: 'black', height: window.screen.height }}>
+			<div className="container">
+				<h1 style={{ textAlign: 'center', color: 'white' }}>My TODO App </h1>
+				<div>
+					<div className="Todo">
+						<div>
+							<br></br>
+							<br></br>
+							<form>
+								<Input
+									color="secondary"
+									style={{ height: '50px', width: '500px', backgroundColor: 'white' }}
+									placeholder="Subject:"
+									onKeyDown={handleKeyDown}
+									onChange={(e) => handleTitle(e.target.value)}
+									value={title}
+									multiple={true}
+								></Input>
+								<br />
+								<Input
+									placeholder="Description:"
+									multiline="true"
+									className="input"
+									style={{
+										alignItems: 'initial',
+										height: '150px',
+										width: '500px',
+										backgroundColor: 'white'
+									}}
+									onKeyDown={handleKeyDown}
+									onChange={(e) => handleData(e.target.value)}
+									value={data}
+								></Input>
+								<br />
+							</form>
+						</div>
 
-					<div>
-						<Button
-							size="small"
-							variant="contained"
-							color="primary"
-							onClick={(e) => {
-								saveItem(e);
-							}}
-						>
-							Save
+						<div>
+							<Button
+								size="small"
+								variant="contained"
+								color="primary"
+								onClick={(e) => {
+									saveItem(e);
+								}}
+							>
+								Save
+							</Button>
+							<br></br>
+
+							<Button
+								size="small"
+								variant="outlined"
+								color="secondary"
+								className="Delete-Button"
+								onClick={(e) => {
+									clearInput(e);
+								}}
+							>
+								Clear
+							</Button>
+							<hr />
+						</div>
+						<div className="container">{showTable()}</div>
+						<hr style={{ width: '50%' }} />
+						<Button size="small" variant="outlined" color="secondary" onClick={(e) => deleteList(e)}>
+							Clear <br></br> List
 						</Button>
-						<br></br>
-						<br></br>
-						<Button
-							size="small"
-							variant="outlined"
-							color="secondary"
-							className="Delete-Button"
-							onClick={(e) => {
-								clearInput(e);
-							}}
-						>
-							Clear
-						</Button>
-						<hr />
-						<p className="Input-todo">{data}</p>
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
 					</div>
-					<div>{showTable()}</div>
-					<hr style={{ width: '50%' }} />
-					<Button size="small" variant="outlined" color="secondary" onClick={(e) => deleteList(e)}>
-						Clear <br></br> List
-					</Button>
 				</div>
-				<h1>programandoconro</h1>
 			</div>
+			<h1 style={{ color: 'white', textAlign: 'center', alignContent: 'baseline' }}>programandoconro</h1>
 		</div>
 	);
 }
