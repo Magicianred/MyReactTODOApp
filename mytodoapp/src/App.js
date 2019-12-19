5e2803b9a13a89cb86d5d106676615ef2f78ec1b
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-19 18:49:43 +0000

    Update App.js

--- mytodoapp/src/App.js
+++ mytodoapp/src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
 import randomColor from 'randomcolor';
-import { Button, Paper, Card, Input, CardContent } from '@material-ui/core';
+import { Button, Paper, Card, CardContent, TextField, Container, Typography } from '@material-ui/core';
 
 function App() {
 	const [title, handleTitle] = useState('');
 	const [data, handleData] = useState('');
 	const [list, setList] = useState([]);
+	const [color, setColor] = useState([]);
 
 	const saveItem = () => {
 		setList((e) => [...e, title + ': ' + data]);
+
+		function chooseColor(array = []) {
+			for (let i = 0; i < color.length + 1; i++) {
+				array.push(randomColor());
+			}
+		}
+
+		const myColors = [];
+		chooseColor(myColors);
+		console.log(myColors);
+		setColor(myColors);
 	};
 
 	const handleKeyDown = (e) => {
@@ -31,38 +43,27 @@ function App() {
 	};
 	const showTable = () => {
 		return (
-			<div>
+			<Container>
 				{list.map((e, k) => (
-					<div>
-						<Paper>
-							<li key={k}>
-								<Card raised="true" width="200px">
-									<CardContent
-										style={{
-											textAlign: 'left',
-											backgroundColor: randomColor(),
-											width: window.screen.width
-										}}
-									>
-										<p>{e}</p>
-									</CardContent>
-								</Card>
+					<Paper key={k} style={{ backgroundColor: 'black' }}>
+						<Card style={{ backgroundColor: color[k] }} raised={true} width="200px">
+							<CardContent
+								style={{
+									textAlign: 'left'
+								}}
+							>
+								<Typography>{e}</Typography>
+							</CardContent>
+						</Card>
 
-								<Button
-									onClick={() => handleRemoveItem(k)}
-									size="big"
-									variant="outlined"
-									color="secondary"
-								>
-									Done
-								</Button>
-							</li>
-						</Paper>
-						<br></br>
-						<br></br>
-					</div>
+						<Button onClick={() => handleRemoveItem(k)} variant="outlined" color="secondary">
+							Done
+						</Button>
+						<br />
+						<br />
+					</Paper>
 				))}
-			</div>
+			</Container>
 		);
 	};
 	const deleteList = () => {
@@ -70,87 +71,70 @@ function App() {
 		console.log(list);
 	};
 	return (
-		<div style={{ backgroundColor: 'black', height: window.screen.height }}>
-			<div className="container">
-				<h1 style={{ textAlign: 'center', color: 'white' }}>My TODO App </h1>
-				<div>
-					<div className="Todo">
-						<div>
-							<br></br>
-							<br></br>
-							<form>
-								<Input
-									color="secondary"
-									style={{ height: '50px', width: '500px', backgroundColor: 'white' }}
-									placeholder="Subject:"
-									onKeyDown={handleKeyDown}
-									onChange={(e) => handleTitle(e.target.value)}
-									value={title}
-									multiple={true}
-								></Input>
-								<br />
-								<Input
-									placeholder="Description:"
-									multiline="true"
-									className="input"
-									style={{
-										alignItems: 'initial',
-										height: '150px',
-										width: '500px',
-										backgroundColor: 'white'
-									}}
-									onKeyDown={handleKeyDown}
-									onChange={(e) => handleData(e.target.value)}
-									value={data}
-								></Input>
-								<br />
-							</form>
-						</div>
+		<Container style={{ backgroundColor: 'black', height: window.screen.height }}>
+			<h1 style={{ textAlign: 'center', color: 'white' }}>My TODO App </h1>
+			<div className="Todo">
+				<Card>
+					<TextField
+						style={{ width: '500px', backgroundColor: 'white' }}
+						placeholder="Subject:"
+						onKeyDown={handleKeyDown}
+						onChange={(e) => handleTitle(e.target.value)}
+						value={title}
+						multiple={true}
+					></TextField>
+					<br />
+					<TextField
+						placeholder="Description:"
+						multiline={true}
+						className="input"
+						style={{
+							alignItems: 'initial',
 
-						<div>
-							<Button
-								size="small"
-								variant="contained"
-								color="primary"
-								onClick={(e) => {
-									saveItem(e);
-								}}
-							>
-								Save
-							</Button>
-							<br></br>
+							width: '500px',
+							backgroundColor: 'white'
+						}}
+						onKeyDown={handleKeyDown}
+						onChange={(e) => handleData(e.target.value)}
+						value={data}
+					></TextField>
+				</Card>
 
-							<Button
-								size="small"
-								variant="outlined"
-								color="secondary"
-								className="Delete-Button"
-								onClick={(e) => {
-									clearInput(e);
-								}}
-							>
-								Clear
-							</Button>
-							<hr />
-						</div>
-						<div className="container">{showTable()}</div>
-						<hr style={{ width: '50%' }} />
-						<Button size="small" variant="outlined" color="secondary" onClick={(e) => deleteList(e)}>
-							Clear <br></br> List
-						</Button>
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-					</div>
-				</div>
+				<Button
+					size="small"
+					variant="contained"
+					color="primary"
+					onClick={(e) => {
+						saveItem(e);
+					}}
+				>
+					Save
+				</Button>
+				<br></br>
+
+				<Button
+					size="small"
+					variant="outlined"
+					color="secondary"
+					className="Delete-Button"
+					onClick={(e) => {
+						clearInput(e);
+					}}
+				>
+					Clear
+				</Button>
+				<hr />
+
+				{showTable()}
+				<hr style={{ width: '50%' }} />
+				<Button size="small" variant="outlined" color="secondary" onClick={(e) => deleteList(e)}>
+					Clear <br></br> List
+				</Button>
+				<br />
 			</div>
+
 			<h1 style={{ color: 'white', textAlign: 'center', alignContent: 'baseline' }}>programandoconro</h1>
-		</div>
+		</Container>
 	);
 }
 
