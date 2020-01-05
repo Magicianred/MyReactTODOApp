1a6d36f1d459b97c96ce0f22509de72f679f2002
sony <sony@localhost.localdomain>
2020-01-05 16:40:26 +0000

    firebase

--- mytodoapp/src/firebase.js
+++ mytodoapp/src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-	apiKey: 'AIzaSyCSQbRH-2FsHE7mHMTZYXptMZKcf_t5wGs',
-	authDomain: 'mytodoapp-9b5fa.firebaseapp.com',
-	databaseURL: 'https://mytodoapp-9b5fa.firebaseio.com',
-	projectId: 'mytodoapp-9b5fa',
-	storageBucket: 'mytodoapp-9b5fa.appspot.com',
-	messagingSenderId: '509159035749',
-	appId: '1:509159035749:web:dbaac8820b3bb1dbe9078c',
-	measurementId: 'G-STHFCELLPN'
+	//add your Firebase credentials here
 };
 
 if (!firebase.apps.length) {
